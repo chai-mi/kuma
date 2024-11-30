246a3a09590c1cffe7905ae37323ae78772dc587
chai-mi <107172917+chai-mi@users.noreply.github.com>
2024-11-30 15:41:49 +0800

    remove

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(request: Request): Promise<Response> {
 		return fetch(request)
 	}
 }
